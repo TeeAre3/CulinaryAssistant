38e3f807fe799cc702bbd6c4d899b3f7c8aa5615
TeeAre3 <TeeAre3@DESKTOP-B1F2J57>
2026-04-20 20:46:31 +0200

    Parsowanie JSON za pomocą JsonExtensionData

--- CulinaryAssistant/Profiles/RecipeProfile.cs
+++ CulinaryAssistant/Profiles/RecipeProfile.cs
@@ -8,32 +8,7 @@ namespace CulinaryAssistant.Profiles
         public RecipeProfile()
         {
             CreateMap<MealDetail, CleanRecipe>()
-                .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src =>
-                    new List<IngredientItem>
-                    {
-                        new IngredientItem { Name = src.Ingredient1, Measure = src.Measure1 },
-                        new IngredientItem { Name = src.Ingredient2, Measure = src.Measure2 },
-                        new IngredientItem { Name = src.Ingredient3, Measure = src.Measure3 },
-                        new IngredientItem { Name = src.Ingredient4, Measure = src.Measure4 },
-                        new IngredientItem { Name = src.Ingredient5, Measure = src.Measure5 },
-                        new IngredientItem { Name = src.Ingredient6, Measure = src.Measure6 },
-                        new IngredientItem { Name = src.Ingredient7, Measure = src.Measure7 },
-                        new IngredientItem { Name = src.Ingredient8, Measure = src.Measure8 },
-                        new IngredientItem { Name = src.Ingredient9, Measure = src.Measure9 },
-                        new IngredientItem { Name = src.Ingredient10, Measure = src.Measure10 },
-                        new IngredientItem { Name = src.Ingredient11, Measure = src.Measure11 },
-                        new IngredientItem { Name = src.Ingredient12, Measure = src.Measure12 },
-                        new IngredientItem { Name = src.Ingredient13, Measure = src.Measure13 },
-                        new IngredientItem { Name = src.Ingredient14, Measure = src.Measure14 },
-                        new IngredientItem { Name = src.Ingredient15, Measure = src.Measure15 },
-                        new IngredientItem { Name = src.Ingredient16, Measure = src.Measure16 },
-                        new IngredientItem { Name = src.Ingredient17, Measure = src.Measure17 },
-                        new IngredientItem { Name = src.Ingredient18, Measure = src.Measure18 },
-                        new IngredientItem { Name = src.Ingredient19, Measure = src.Measure19 },
-                        new IngredientItem { Name = src.Ingredient20, Measure = src.Measure20 }
-                    }
-                    .Where(i => !string.IsNullOrWhiteSpace(i.Name)).ToList()
-                ));
+                .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.ExtractIngredients()));
         }
     }
 }
